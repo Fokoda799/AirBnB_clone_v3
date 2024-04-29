70a3c00df90981c0420e0dd3eeec3996857311cb
Fokoda799 <feyt2003@gmail.com>
2024-04-29 09:50:22 +0100

    ok

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Flask app module"""
+"""Flask app"""
 from api.v1.views import app_views
 from flask import Flask, jsonify
-from models import storage
 import os
+from models import storage
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -17,11 +17,11 @@ def teardown(exception):
 
 @app.errorhandler(404)
 def not_found(error):
-    """Shows not found page"""
+    """Not found error"""
     return jsonify({"error": "Not found"})
 
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
+    host = os.getenv("HBNB_API_HOST", '0.0.0.0')
+    port = int(os.getenv("HBNB_API_PORT", 5000))
     app.run(debug=True, host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Status of my API"""
+"""Main page"""
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
